6530f638dad981fdcc7f84411be37b9792c4d29f
Angelica Panchito <angelicapanchito777@gmail.com>
2025-08-12 08:22:24 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/icons/logo.tsx
+++ src/components/icons/logo.tsx
@@ -13,7 +13,7 @@ export const Logo = ({ className }: { className?: string }) => (
   >
     <path d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
     <path d="M12 6V12L16 14" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-    <path d="M19.07 4.93005L18.63 5.37005" stroke="hsl(var(--accent))" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"/>
-    <path d="M4.93005 19.07L5.37005 18.63" stroke="hsl(var(--accent))" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M19.07 4.93005L18.63 5.37005" stroke="hsl(var(--accent))" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"/>
+    <path d="M4.93005 19.07L5.37005 18.63" stroke="hsl(var(--accent))" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"/>
   </svg>
 );
